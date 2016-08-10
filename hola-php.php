6463aa10f7db8d4078965276a9687d81b89d8317
megumi <megumi.suki.10@gmail.com>
2016-08-09 19:22:17 -0500

    Agregamos presentacion y comportamiento

--- hola-php.php
+++ hola-php.php
@@ -2,10 +2,41 @@
 <html lang="es">
 	<head>
 		<title>
-			MEGUMI
+			MEGUMI hola PHP
 		</title>
+		<style type="text/css">/*presentacion*/
+			h1{color:peru;}
+			p{color:#adcbf7;}
+			li:first-letter{font-size: 2em;}
+		</style>
+													<!--http://getbootstrap.com/getting-started/-->
+		<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
 	</head>
 	<body>
+		<script type="text/javascript">
+			alert("hola "+prompt("ingresa tu nombre "));
+		</script>
 		<h1>HOLA PHP</h1>
+		<p>Dias de la semana</p>
+		<a class="btn btn-primary" href="http://www.google.com">Ir a google</a>
+		<a class="btn btn-primary" href="https://www.youtube.com">Ir a youtube</a>
+		<ol>
+			<li>Lunes</li>
+			<li>Martes</li>
+			<li>Miercoles</li>
+			<li>Jueves</li>
+			<li>Viernes</li>
+			<li>Sabado</li>
+			<li>Domingo</li>
+		</ol>
+		
 	</body>
 </html>
