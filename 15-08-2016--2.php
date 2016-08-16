a4c87bff2d48d90f0a077f54983e8bc961a5ced8
megumi <megumi.suki.10@gmail.com>
2016-08-15 19:24:33 -0500

    variables

---
+++ 15-08-2016--2.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="es">
+	<head>
+		<title></title>
+	</head>
+	<body>
+	<?php
+		/*********************variables***********************/
+		//asignar valores a variables
+		$nombre="Juan";
+		//las variables en php inician con el simbolo " $ "
+		echo "<p>Hola ".$nombre."</p>";
+		echo "<p>Hola $nombre</p>";//el contenido entre " " se interpreta
+		echo '<p>Hola $nombre</p>';//el contenido entre ' ' solo se muestra
+		unset($nombre);
+		echo "<p>Hola ".$nombre."</p>";
+		echo "<p>Hola $nombre</p>";//el contenido entre " " se interpreta
+		echo '<p>Hola $nombre</p>';//el contenido entre ' ' solo se muestra
+
+		$nombre="brayan";
+		echo "<p>Hola $nombre</p>";
+		$nombre="null";//null es un valor especial nulo
+		echo "<p>Hola $nombre</p>";
+
+		$nombre="Juan";
+		var_dump($nombre);
+		$edad=18;
+		var_dump($edad);
+		$edad="18";
+		var_dump($edad);
+		var_dump(null);
+
+		/********TIPOS DE VARIABLES**********/
+		//BOOLEANO
+		$esEstudiante=TRUE;
+		$esVaron=FALSE;
+		//ENTERO
+		$edad=20;
+		//punto flotante
+		$valor_pi=3.1416;
+		//cadena de texto
+		$nombre="Jose Carlos";
+		//NULO
+		$semestre=null;
+
+	?>
+	</body>
+</html>
---
+++ 15-08-2016.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title></title>
+	</head>
+	<body>
+	<?php 
+		echo "<p>Hola php</p>";
+		echo "<p>Hola\n php</p>";
+		echo "<p>Hola\t php</p>";
+		echo "<p>Hola
+
+
+
+
+		php</p>";
+		echo "<p>Hola                                                                                                                                                            php</p>";
+		echo "\n\n";
+
+
+	echo "<ul>";
+	echo "<li>1</li>";
+	echo "<li>2</li>";
+	echo "<li>3</li>";
+	echo "</ul>";
+		echo "\n\n";
+
+	
+	echo "<ul>\n\t<li>1</li>\n\t<li>2</li>\n\t<li>3</li>\n</ul>";
+		echo "\n\n";
+
+
+	echo "<ul>
+		<li>1</li>
+		<li>2</li>
+		<li>3</li>
+	</ul>";
+	
+	echo "<br>";
+
+	echo "<p>Hola \"PHP\"</p>";
+
+	echo "<p>Hola 'php'</p>";
+	echo '<p>Hola "php"</p>';
+	/*comentario de varias lineas*/
+	//comentario de una linea
+
+	?>
+
+
+
+	</body>
+</html>
