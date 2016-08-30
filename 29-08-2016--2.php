e93b1ab542a9401f056e98bf8d5e176751951577
megumi <megumi.suki.10@gmail.com>
2016-08-29 19:15:51 -0500

    menus

---
+++ 29-08-2016--2.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title></title><!--archivo reutilizable-->
+	</head>
+	<body>
+	<h1>Inclusion de archivos</h1>
+	<p>Contenido Principal</p>
+	<p>hola</p>
+	</body>
+</html>
